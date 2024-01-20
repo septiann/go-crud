9eb42d016d1e65cec3ad2b4a9af30882d943966a
Septian Nugraha <septiann508@gmail.com>
2024-01-20 23:39:22 +0700

    add validation and delete build exe project

--- controllers/postController.go
+++ controllers/postController.go
@@ -17,6 +17,7 @@ func PostCreate(c *gin.Context) {
 
 	c.Bind(&body)
 
+	// Validation
 	if body.Title == "" {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Title is required",
@@ -35,13 +36,16 @@ func PostCreate(c *gin.Context) {
 	result := initializers.DB.Create(&post)
 
 	if result.Error != nil {
-		c.Status(400)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": result.Error,
+			"status":  "E",
+		})
 
 		return
 	}
 
 	// Return data
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"message": "Created post successfully",
 		"status":  "S",
 	})
@@ -52,8 +56,18 @@ func PostList(c *gin.Context) {
 	var posts []models.Post
 	initializers.DB.Find(&posts)
 
+	// Validate if post is empty
+	if len(posts) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "No posts found",
+			"status":  "E",
+		})
+
+		return
+	}
+
 	// Response data
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"data": posts,
 	})
 }
@@ -66,8 +80,18 @@ func PostShow(c *gin.Context) {
 	var posts models.Post
 	initializers.DB.First(&posts, id)
 
+	// Validation
+	if posts.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Post not found",
+			"status":  "E",
+		})
+
+		return
+	}
+
 	// Response data
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"data": posts,
 	})
 }
@@ -84,18 +108,37 @@ func PostUpdate(c *gin.Context) {
 
 	c.Bind(&body)
 
+	// Validation
+	if body.Title == "" && body.Body == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Please insert Title or Body",
+			"status":  "E",
+		})
+
+		return
+	}
+
 	// Find the post
 	var post models.Post
 	initializers.DB.First(&post, id)
 
 	// Update Data
-	initializers.DB.Model(&post).Updates(models.Post{
+	result := initializers.DB.Model(&post).Updates(models.Post{
 		Title: body.Title,
 		Body:  body.Body,
 	})
 
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": result.Error,
+			"status":  "E",
+		})
+
+		return
+	}
+
 	// Response
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"message": "Update post successfully",
 		"status":  "S",
 	})
@@ -105,12 +148,34 @@ func PostDelete(c *gin.Context) {
 	// Get ID from query param
 	id := c.Param("id")
 
+	// Validation
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Please insert ID",
+			"status":  "E",
+		})
+
+		return
+	}
+
+	var checkPost models.Post
+	initializers.DB.First(&checkPost, id)
+
+	if checkPost.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Post not found",
+			"status":  "E",
+		})
+
+		return
+	}
+
 	// Delete the post
 	var post models.Post
-	initializers.DB.Delete(&post, id)
+	initializers.DB.Unscoped().Delete(&post, id)
 
 	// Response
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"message": "Delete post successfully",
 		"status":  "S",
 	})
